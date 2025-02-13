dd2fc2c4461ad826939b142580dbde6c87b2e4ea
Luis Banck <luisbanck@Luiss-iMac.lan>
2025-02-13 14:24:07 -0600

    Fix Next.js export issue and specify Node version

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Esto es clave para que Vercel genere una versión estática
-}
+  output: "export",
+  reactStrictMode: true,
+};
 
-module.exports = nextConfig;
+export default nextConfig;
